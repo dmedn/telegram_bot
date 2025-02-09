125df2957bde2dfc02586cf67c809d3b52b355db
mikhail <Darkmedan@yandex.ru>
2025-02-09 23:46:54 +0300

    default handlers(update)

---
+++ handlers/callback_handlers/choice_city.py
@@ -0,0 +1,16 @@
+from loguru import logger
+from loader import bot
+from telebot.types import CallbackQuery
+from states.user_states import UserInputState
+
+
+@bot.callback_query_handler(func=lambda call: call.data.isdigit())
+def destination_id_callback_handler(call: CallbackQuery) -> None:
+    logger.info(f'Пользователь {call.message.chat.id} выбрал город')
+    if call.data:
+        bot.set_state(call.message.chat.id, UserInputState.destination_id)
+        with bot.retrieve_data(call.message.chat.id) as data:
+            data['destination_id'] = call.data
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        bot.set_state(call.message.chat.id, UserInputState.quantity_hotels)
+        bot.send_message(call.message.chat.id, 'Какое количество отелей вы можете найти?')
---
+++ handlers/default_handlers/__init__.py
@@ -0,0 +1,4 @@
+from . import echo
+from . import start
+from . import help
+
